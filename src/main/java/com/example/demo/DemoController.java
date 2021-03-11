ea296d3ef5e5d1ca3b3094686e49412224faac30
Sandy Raj <sandhia91@gmail.com>
2021-03-11 13:16:28 +0530

    first commit

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import java.util.*;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class DemoController {
+
+	@RequestMapping(value = "/getInfo", method = RequestMethod.GET)  
+	public List<String> getAllBooks()   
+	{  
+	 System.out.println("Inside method");
+	 List<String> listString = new ArrayList<String>();
+	 listString.add("Cindrella");
+	 listString.add("Snowhite");
+	 listString.add("How To Fall in Love");
+	 System.out.println("Ending method");
+	 return listString;
+	}  
+}
