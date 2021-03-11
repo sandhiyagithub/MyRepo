3e46f993298f88e57d07c1bfededa910c847a3cf
sandh <sandhia91@gmail.com>
2021-03-11 13:29:40 +0530

    commit

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
+	
+	//add to commit
 	@RequestMapping(value = "/getInfo", method = RequestMethod.GET)  
 	public List<String> getAllBooks()   
 	{  
